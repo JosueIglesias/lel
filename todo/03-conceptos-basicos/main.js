bfd4141e680ecb5167b5f99fb4113c78d37799c5
Josue Iglesias <jiglesias3@ucol.mx>
2019-09-28 20:46:51 -0500

    primer commit

---
+++ todo/03-conceptos-basicos/main.js
@@ -0,0 +1,48 @@
+// I am a comment
+/*
+hola amigos de youtube el dia de hoy iremos a pasiar al pinar de chayanne en donde probaremos de su exquisito menu y sus precios para saber si es caro o barato ir a pasiar a lla jajajajajajajajaj xdxdxdxdxdddddddd
+*/
+var numero
+var numeroCuenta
+var numeroCuentaEmpleado
+
+var nombre = 'Juan Perez'
+var edad = 22
+var email
+var esEstudiante = true
+var numero1 = 5
+var numero2= 2
+
+console.log(numero1)
+numero1 = numero1 + 1
+console.log(numero1)
+numero1++
+console.log(numero1)
+numero1 = numero1 - 1
+console.log(numero1)
+numero1--
+console.log(numero1)
+
+console.log(50 + 10 / 8 + 2)
+console.log((50 + 10) / (8 + 2))
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 % numero2)
+
+console.log(
+    nombre + ' tiene ' + edad + ' años. ¿Es estudiante? ' + esEstudiante 
+)
+
+//Ejemplo de conceptos basicos
+console.log('Hola, mundo!')
+console.log(nombre)
+console.log(edad)
+console.log(email)
+
+edad = 'veintidos'
+console.log(edad)
+
+nombre = prompt('¿Cómo te llamas ?')
+alert('Hola ' + nombre)
